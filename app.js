27697f93f929a5aa53dac180bfbf01e8646cb48b
mascaryu <mascarenhasyuri@hotmail.com>
2024-01-09 20:52:53 -0300

    Add clear input after search

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Main App
 const form = document.querySelector("#search-form");
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
@@ -9,12 +10,18 @@ form.addEventListener("submit", async (e) => {
   };
   const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
   showImages(res.data);
+  clearInput();
 });
 
+// Other Functions
 const showImages = (shows) => {
   for (let curr of shows) {
     const img = document.createElement("IMG");
     img.src = curr.show.image.medium;
     document.body.append(img);
   }
+};
+
+const clearInput = () => {
+  form.elements.query.value = "";
 };
